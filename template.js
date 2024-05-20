f7ae211468deb32838f09b78c4177b0d1f093fda
Kosuke <123919416+KISETU-ggwp@users.noreply.github.com>
2024-05-20 09:28:46 +0000

    FISH_fix_06

--- template.js
+++ template.js
@@ -1,38 +1,28 @@
-// // テンプレの動きをしてする動きを決める
-// function openwin() {
-//   window.open("template.html", "", "width=400,height=200,top=600,left=600");
-// }
-
 //設定した定型文を保存する
 function savetmp() {
   //テキストエリアから値を取得
   var templateText = document.getElementById("addTemplate").value;
+  console.log(templateText);
 
   //ラジオボタンを生成して追加
   var radioButtonsContainer = document.getElementById("radioButtonsContainer");
   var radioButton = document.createElement("input");
-  radioButton.type = "radio";//変更Point
+  radioButton.type = "radio";
   radioButton.name = "template";
 
   var formattedTemplate = templateText.replace(/<br>/g, "\n");
 
+  //ラジオボタンに対応するラベルを生成して追加
   var lebelElement = document.createElement("label");
-  HTMLLabelElement.innerHTML = formattedTemplate;
+  lebelElement.innerHTML = formattedTemplate;
 
   var containterElement = document.createElement("div");
-  containterElement.appendChild(radioButton);//変更するPoint
+  containterElement.appendChild(radioButton);
   containterElement.appendChild(lebelElement);
 
   radioButtonsContainer.appendChild(containterElement);
 
-  //onchangeイベントハンドラを関数に直接割り当てる
-  radioButton.onchange = tmpbutton;
-  radioButtonsContainer.appendChild(radioButton);
-
-  //ラジオボタンに対応するラベルを生成して追加
-  var label = document.createElement("label");
-  label.innerText = templateText;
-  radioButtonsContainer.appendChild(label);
+  //radioButtonsContainer.appendChild(label);
 
   //textareaとの重複を防ぐ改行
   var newline = document.createElement('br');
@@ -42,9 +32,20 @@ function savetmp() {
   const textarea = document.getElementById("addTemplate");
 
   textarea.value = "";
+
+  //動的に音声発出ボタンを作成
+  var speechReady = function(event) {
+    var utterance = new SpeechSynthesisUtterance(event.target.nextSibiling.formattedTemplate);
+    utterance.lang = "ja-JP";
+    utterance.rate = 0.7;
+    window.speechSynthesis.speak(utterance);
+  }
+  console.log(formattedTemplate);
+  //ラジオボタンにクリックイベントリスナーを追加
+  radioButton.addEventListener('click', speechReady);
 }
 
-
+// 既に登録している定型文を読み上げる
 // 各ボタンに対してイベントリスナーを追加
 document.getElementById('speakerButton1').addEventListener('click', function(e) {
   readText(e);
@@ -61,7 +62,6 @@ document.getElementById('speakerButton3').addEventListener('click', function(e) 
 document.getElementById('speakerButton4').addEventListener('click', function(e) {
   readText(e);
 });
-
 
 // テキストを読み上げる関数
 function readText(e) {
@@ -81,3 +81,44 @@ function readText(e) {
   // テキストを読み上げる
   window.speechSynthesis.speak(utterance);
 }
+
+document.getElementById('speakerButton5').addEventListener('click', function(t) {
+  readTmp(t);
+});
+
+document.getElementById('speakerButton6').addEventListener('click', function(t) {
+  readTmp(t);
+});
+
+document.getElementById('speakerButton7').addEventListener('click', function(t) {
+  readTmp(t);
+});
+
+document.getElementById('speakerButton8').addEventListener('click', function(t) {
+  readTmp(t);
+});
+
+document.getElementById('speakerButton9').addEventListener('click', function(t) {
+  readTmp(t);
+});
+
+//テキストエリア内に打ち込まれた定型文を読み上げる関数
+function readTmp(t) {
+  // クリックされたspeakerButtonのidを取得
+  var speakerButtonId = t.target.id;
+
+  // 対応するテキストエリアのidを構築
+  var textareaId = 'tempText' + speakerButtonId.charAt(speakerButtonId.length - 1);
+
+  // 対応するテキストエリアの内容を取得
+  var tempText = document.getElementById(textareaId).value;
+  // SpeechSynthesisUtterance オブジェクトを作成
+  const utterance = new SpeechSynthesisUtterance(tempText);
+  utterance.lang = "ja-JP"; // 言語設定を日本語に設定
+  utterance.rate = 0.7;
+  // テキストを読み上げる
+  window.speechSynthesis.speak(utterance);
+}
+
+
+
